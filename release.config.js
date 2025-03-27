cfa821a92a44af9e1fd4b41fca6a1b0f4ae353ab
Dion Whitehead <260221+dionjwa@users.noreply.github.com>
2025-03-27 11:19:23 -0700

    test

--- release.config.js
+++ release.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  branches: [
+    'main',  // regular releases
+    {name: 'beta', prerelease: true}, // beta releases
+    {name: 'alpha', prerelease: true}  // alpha releases
+  ],
   branches: [{name:"release"}, { name: "metapage-custom-plugins", channel: "alpha", prerelease: "alpha"}],
 };
